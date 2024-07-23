047d20456684efa385a882fb5d6fd60010a6ac90
baltazarottonello <baltazarottonello@gmail.com>
2024-07-23 00:07:29 -0300

    feat signup

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { usersProviders } from './users.providers';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
-  exports: [],
+  controllers: [UsersController],
+  providers: [UsersService, ...usersProviders], //spread operator ... (expands an existing array "usersProviders")
+  exports: [UsersService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,2 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/sequelize';
+import { Inject, Injectable } from '@nestjs/common';
+import { User } from './users.entity';
+import { Constants } from '../utils/constants';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @Inject('USERS_REPOSITORY')
+    private readonly usersRepository: typeof User, //TODO: BaseService for CRUD in DB so we apply DRY principle and make the Service more readable
+  ) {}
+  async create(entity: any): Promise<Boolean> {
+    const hashedPassword = await bcrypt.hash(
+      entity.password,
+      Constants.SALT_OR_ROUNDS,
+    );
+    entity['password'] = hashedPassword;
+    await this.usersRepository.create(entity);
+    return true;
+  }
+
+  async find(entity: any): Promise<User> {
+    const result = await this.usersRepository.findOne({
+      where: {
+        email: entity.email,
+      },
+    });
+    return result;
+  }
+}
